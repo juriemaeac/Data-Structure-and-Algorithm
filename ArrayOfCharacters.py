8632272763a68c6eec1a05bf917a513a950c8cbf
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2020-10-26 14:53:22 +0800

    Array of Characters

--- ArrayOfCharacters.py
+++ ArrayOfCharacters.py
@@ -2,6 +2,7 @@ x = input("Enter a word: ")
 
 #x[::-1] is used to reverse all the elements
 #source lesson: https://jakevdp.github.io/PythonDataScienceHandbook/02.02-the-basics-of-numpy-arrays.html
+
 y = x[::-1]
 
 if x == y:
