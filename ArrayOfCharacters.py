6cc23f344c386132a15fc8ca548858f510cf29cf
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2020-10-26 14:52:09 +0800

    Array of Elements

---
+++ ArrayOfCharacters.py
@@ -0,0 +1,10 @@
+x = input("Enter a word: ")
+
+#x[::-1] is used to reverse all the elements
+#source lesson: https://jakevdp.github.io/PythonDataScienceHandbook/02.02-the-basics-of-numpy-arrays.html
+y = x[::-1]
+
+if x == y:
+    print(x, "is a palindrome")
+else:
+    print(x, "is not a palindrome")
