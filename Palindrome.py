5549b0476b45f64402d6126f77d7e41e3c61d35a
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-01-23 02:13:36 +0800

    with user handling

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,20 @@
+'''
+by Jurie Mae Castronuevo
+from BSCOE 2-6
+'''
+import re
 x = input("Enter a word: ")
 
 #x[::-1] is used to reverse all the elements
 #source lesson: https://jakevdp.github.io/PythonDataScienceHandbook/02.02-the-basics-of-numpy-arrays.html
 
-y = x[::-1]
-
-if x == y:
-    print(x, "is a palindrome")
+#library that accepts word with some symbols and spaces only 
+# since some of names/ words are with symbols
+if re.match(r"^[A-Za-z -.]+$", x):
+    y = x[::-1]
+    if x == y:
+        print(x, "is a palindrome")
+    else:
+        print(x, "is not a palindrome")
 else:
-    print(x, "is not a palindrome")
+    print ("This is not a word")
