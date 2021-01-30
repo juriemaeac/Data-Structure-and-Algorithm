1f067286187de6d625b672bb8ccc1a2f015a1f9c
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-01-30 12:22:07 +0800

    handling

--- AscendingOrder.py
+++ AscendingOrder.py
@@ -1,25 +1,32 @@
+'''
+by Jurie Mae Castronuevo
+from BSCOE 2-6
+'''
 #numbers input split by space
-num = input("Enter 10 numbers:\n").split(' ') 
+num = input("Enter 10 numbers:").split(' ') 
 
-#convert into integer
-arr = [int(num) for num in num]
+try:
+  #convert into integer
+  arr = [int(num) for num in num]
 
-#used to count number of elements in an array
-elements = len(arr)
+    #used to count number of elements in an array
+  elements = len(arr)
+  if elements == 10:
+      #Sort the array in ascending order 
+      temp = 0; 
+      for i in range(0, len(arr)):    
+          for j in range(i+1, len(arr)):    
+              if(arr[i] > arr[j]):    
+                  temp = arr[i];    
+                  arr[i] = arr[j];    
+                  arr[j] = temp;    
+      
+      #Sorted Elements   
+      print("Elements value of array in ascending order: ");    
+      for i in range(0, len(arr)):    
+          print(arr[i], end=" ");  
+  else:
+      print("Invalid. Please enter 10 numbers.")
 
-if elements == 10:
-    #Sort the array in ascending order 
-    temp = 0; 
-    for i in range(0, len(arr)):    
-        for j in range(i+1, len(arr)):    
-            if(arr[i] > arr[j]):    
-                temp = arr[i];    
-                arr[i] = arr[j];    
-                arr[j] = temp;    
-     
-    #Sorted Elements   
-    print("Elements value of array in ascending order: ");    
-    for i in range(0, len(arr)):    
-        print(arr[i], end=" ");  
-else:
-    print("Invalid. Please enter 10 numbers.")
+except: 
+  print("Invalid input! Please enter type:integers only!") 
