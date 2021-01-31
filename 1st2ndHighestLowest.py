45115bd869179af878337afcdcf4a24725b57cb8
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-02-01 02:31:36 +0800

    lab1 prob3

--- 1st2ndHighestLowest.py
+++ 1st2ndHighestLowest.py
@@ -1,39 +1,47 @@
+'''
+by Jurie Mae Castronuevo
+from BSCOE 2-6
+'''
 # #numbers input split by space
 num = input("Enter 10 numbers:\n").split(' ') 
 
-#convert into integer, stored in an array
-arr = [int(num) for num in num]
+try:
+    #convert into integer, stored in an array
+    arr = [int(num) for num in num]
 
-#used to count number of elements in an array
-elements = len(arr)
+    #used to count number of elements in an array
+    elements = len(arr)
 
-if elements == 10:
-    #Sort the array in ascending order 
-    temp = 0; 
-    for i in range(0, len(arr)):    
-        for j in range(i+1, len(arr)):    
-            if(arr[i] > arr[j]):    
-                temp = arr[i];    
-                arr[i] = arr[j];    
-                arr[j] = temp;  
+    if elements == 10:
+        #Sort the array in ascending order 
+        temp = 0; 
+        for i in range(0, len(arr)):    
+            for j in range(i+1, len(arr)):    
+                if(arr[i] > arr[j]):    
+                    temp = arr[i];    
+                    arr[i] = arr[j];    
+                    arr[j] = temp;  
 
-    #dictionaries can't have duplicated keys
-    duplicated = list(dict.fromkeys(arr))
-    print (duplicated)
+        #dictionaries can't have duplicated keys
+        duplicated = list(dict.fromkeys(arr))
+        print (duplicated)
 
-    #used to count number of elements in filtered list
-    repeated = len(duplicated)
+        #used to count number of elements in filtered list
+        repeated = len(duplicated)
 
-    if repeated == 1:
-        print("\nfirst to the highest: ", duplicated[0]);   
-        print("second to the highest: ", duplicated[0]); 
-        print("second to the lowest: ", duplicated[0]); 
-        print("first to the lowest: ", duplicated[0]); 
+        if repeated == 1:
+            print("\nfirst to the highest: ", duplicated[0]);   
+            print("second to the highest: ", duplicated[0]); 
+            print("second to the lowest: ", duplicated[0]); 
+            print("first to the lowest: ", duplicated[0]); 
+        else:
+            #use negative indices to get the end of an array      
+            print("\nfirst to the highest: ", duplicated[-1]);   
+            print("second to the highest: ", duplicated[-2])
+            print("second to the lowest: ", duplicated[1])
+            print("first to the lowest: ", duplicated[0])
     else:
-        #use negative indices to get the end of an array      
-        print("\nfirst to the highest: ", duplicated[-1]);   
-        print("second to the highest: ", duplicated[-2])
-        print("second to the lowest: ", duplicated[1])
-        print("first to the lowest: ", duplicated[0])
-else:
-    print("Please input 10 numbers!")
+        print("Please input 10 numbers!")
+        
+except: 
+  print("Invalid input! Please enter type:integers only!") 
