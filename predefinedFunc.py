08352584b5312844b6116d5f448a7c2e799aa473
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-02-01 03:06:35 +0800

    lab2 prob3

---
+++ predefinedFunc.py
@@ -0,0 +1,14 @@
+'''
+by Jurie Mae Castronuevo
+from BSCOE 2-6
+'''
+
+x = max(3, 60)
+
+def main():
+    print("The greatest out of two numbers is",x)
+    
+if __name__=="__main__":
+    main()
+    
+
