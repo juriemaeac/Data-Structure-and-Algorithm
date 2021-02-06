f548da8e1f6211c474e2ae4cd1ec7f0765b1029d
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-02-06 10:25:44 +0800

    lab2 prob3

--- predefinedFunc.py
+++ predefinedFunc.py
@@ -2,13 +2,19 @@
 by Jurie Mae Castronuevo
 from BSCOE 2-6
 '''
+n1 = input("Enter number1: ")
+if n1.isdigit():
+    n2 = input("Enter number2: ")
+    if n2.isdigit():
+        x = max(n1, n2)
 
-x = max(3, 60)
-
-def main():
-    print("The greatest out of two numbers is",x)
-    
-if __name__=="__main__":
-    main()
-    
+        def main():
+            print("The greatest out of two numbers is",x)
+            
+        if __name__=="__main__":
+            main()
+    else:
+        print("Invalid Input. Integers only.")
+else:
+    print("Invalid Input. Integers only.")
 
