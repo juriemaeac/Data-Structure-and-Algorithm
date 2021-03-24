868697d15c353048a29eeeb06503844edf7dbc77
Jurie Mae Castronuevo <juriemaeac@users.noreply.github.com>
2021-03-24 13:00:14 +0800

    final dsa

---
+++ Lab Exercise 4/strcpy.py
@@ -0,0 +1,19 @@
+'''
+by Jurie Mae Castronuevo
+from BSCOE 2-6
+[February 17, 2021]
+'''
+
+def strcpy(destination, source):
+    destination = source
+    
+def main():
+    str1 = 'copy'
+    str2 = input('Enter something: ')
+    strcpy(str1, str2)
+    print(str1)
+    pass
+
+if __name__ == '__main__':
+    main()
+    
